93e0a88c214b91345156de1562f4378a5d8baff0
Force_human <66125890+Ffferrar@users.noreply.github.com>
2021-12-05 03:33:24 +0300

    solution4

--- src/singleton.java
+++ src/singleton.java
@@ -1,2 +1,30 @@
-public class singleton {
+public final class singleton {
+    private static volatile singleton instance;
+    String value;
+
+    private singleton(String value)
+    {
+        // Этот код эмулирует медленную инициализацию
+        try
+        {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException ex)
+        {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    static singleton getInstance(String value)
+    {
+        if (instance == null){
+            synchronized (singleton.class){
+                if (instance == null){
+                    instance = new singleton(value);
+                }
+            }
+        }
+        return instance;
+    }
 }
